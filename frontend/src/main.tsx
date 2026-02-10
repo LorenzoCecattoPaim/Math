f2b80fec875d9eda34f493769227aa998b4652da
LorenzoCecattoPaim <cecattopaiml@gmail.com>
2026-02-09 21:17:19 -0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ import "./index.css";
 
 const queryClient = new QueryClient();
 
-console.log("API BASE:", import.meta.env.VITE_API_URL);
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
